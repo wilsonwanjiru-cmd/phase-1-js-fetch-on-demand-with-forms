8c874db45bfe2a3e6f68af53adc294bf64127617
Wilson Muita Wanjiru <wilsonmuita41@gmail.com>
2023-07-11 13:12:51 +0300

    <Fetch on Demand with Forms>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 const init = () => {
+    const inputForm = document.querySelector('form')
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error('Movie not found');
+          }
+        })
+        .then(data => {
+          const title = document.querySelector('section#movieDetails h4');
+          const summary = document.querySelector('section#movieDetails p');
+  
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+        })
+        .catch(error => {
+          console.error(error);
+          // Handle the error
+        });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
+  
+  
+  
+  
